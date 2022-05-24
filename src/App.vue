17e63988af660ebfbf02a6a0a5673e858d797e83
bona <brunobonarelli@gmail.com>
2022-05-23 23:20:36 -0300

    V 1.2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormularioWeb></FormularioWeb>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormularioWeb from './components/Formulario-Validado.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormularioWeb,
   }
 }
 </script>
